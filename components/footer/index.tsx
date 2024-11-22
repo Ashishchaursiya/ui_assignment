f65303839e1fd6e8ca34a3830108b53d30e1f7c0
Ashishchaursiya <chaurasiyaashish383@gmail.com>
2024-11-22 09:41:10 +0530

    improve footer ui

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -7,10 +7,10 @@ interface FooterLinkProps {
 }
 
 const FooterLink: React.FC<FooterLinkProps> = ({ imageSrc, altText, title }) => (
-  <div className="sm:col-span-2 flex flex-col items-start">
+  <div className="mt-3 col-span-6 sm:col-span-2 flex flex-col items-start">
     <Image src={imageSrc} alt={altText} width={40} height={40} className="mb-2" />
     <p className="font-poppins text-[14px] font-black leading-[21px] text-left">{title}</p>
-    <div className="flex items-center">
+    <div className="mt-2 flex items-center">
       <p className="font-poppins text-[14px] font-medium leading-[21px] text-left inline">바로가기 </p>
       <Image src="/images/arrow.png" alt="arrow" width={20} height={20} className="mb-2 inline" />
     </div>
@@ -22,7 +22,7 @@ export default function Footer() {
     <>
       <div className="mx-auto max-w-6xl px-2 sm:px-6 lg:px-8 sm:mt-20">
         <div className="grid sm:grid-cols-12 p-3 gap-x-3">
-          <div className="sm:col-span-3">
+          <div className="col-span-12 sm:col-span-3">
             <Image src="/images/flogo.png" alt="footer logo" width="187" height="34" className="mb-2" />
             <p className="font-poppins text-sm font-black leading-[21px] text-left">
               우리는 국가의 장벽을 넘어 최고의 인재를 매칭해드립니다.
@@ -42,23 +42,23 @@ export default function Footer() {
           <FooterLink imageSrc="/images/setting.png" altText="setting image" title="해외 개발자 활용 서비스" />
         </div>
 
-        <div className="grid sm:grid-cols-12 p-3 gap-x-5">
-          <div className="sm:col-span-3">
+        <div className="grid grid-cols-12 p-3 gap-x-5">
+          <div className="col-span-8 sm:col-span-3">
             <p className="font-poppins text-[12px] font-black leading-[18px] text-left">상호명 </p>
             <p className="font-poppins text-[13px] leading-[18px] text-left">하이퍼하이어</p>
             <p className="font-poppins text-[13px] font-black leading-[18px] text-left">Hyperhire India Private Limited</p>
           </div>
-          <div className="sm:col-span-2">
+          <div className="col-span-4 sm:col-span-2">
             <p className="font-poppins text-[12px] font-black leading-[18px] text-left">대표 CEO </p>
             <p className="font-poppins text-[13px] leading-[18px] text-left">김주현</p>
             <p className="font-poppins text-[13px] font-black leading-[18px] text-left">Juhyun Kim</p>
           </div>
-          <div className="sm:col-span-2">
+          <div className="col-span-12 sm:col-span-2 my-3">
             <p className="font-poppins text-[12px] font-black leading-[18px] text-left">사업자등록번호 CIN </p>
             <p className="font-poppins text-[13px] leading-[18px] text-left">427-86-01187</p>
             <p className="font-poppins text-[13px] font-black leading-[18px] text-left">U74110DL2016PTC290812 </p>
           </div>
-          <div className="sm:col-span-5">
+          <div className="col-span-12 sm:col-span-5 my-3">
             <p className="font-poppins text-[12px] font-black leading-[18px] text-left">주소 ADDRESS </p>
             <p className="font-poppins text-[13px] leading-[18px] text-left">서울특별시 강남대로 479, 지하 1층 238호</p>
             <p className="font-poppins text-[13px] font-black leading-[18px] text-left">
